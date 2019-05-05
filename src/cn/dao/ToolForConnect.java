c1ca337f1403fd86de39ce8b21c48169550b3bfa
liusd <ayusue@163.com>
2019-05-05 17:30:36 +0800

    Add Connection Tool

---
+++ src/cn/dao/ToolForConnect.java
@@ -0,0 +1,7 @@
+package cn.dao;
+
+public class ToolForConnect {
+	public static void Show() {
+		System.out.println("Connection Succeeded");
+	}
+}
--- src/cn/liusd/HelloWorld.java
+++ src/cn/liusd/HelloWorld.java
@@ -1,7 +1,10 @@
 package cn.liusd;
 
+import cn.dao.*;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
+		ToolForConnect.Show();
 	}
 }
