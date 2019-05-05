245585a11f853a895c85baf6d674f6a0f1340175
liusd <ayusue@163.com>
2019-05-05 17:27:20 +0800

    Add ToolForFile

---
+++ src/cn/liusd/ToolForFile.java
@@ -0,0 +1,19 @@
+package cn.liusd;
+
+import java.io.*;
+
+public class ToolForFile {
+	public static void Create(String path) {
+		File file = new File(path);
+		if(file.exists()) {
+			return;
+		}else {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
